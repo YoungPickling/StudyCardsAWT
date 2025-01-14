a07f76d965a351100e0b26b57e306c21c5545c20
Maksim Pavlenko <maksimas.pavlenko@gmail.com>
2025-01-15 00:02:04 +0200

    feat: menu bar

--- src/main/java/lt/study/StudyMain.java
+++ src/main/java/lt/study/StudyMain.java
@@ -1,38 +1,59 @@
 package lt.study;
 
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class StudyMain {
+    private Frame mainFrame;
+    private Label headerLabel;
+    private Label statusLabel;
+    private Panel controlPanel;
 
-    int windowSizeX = 500;
-    int windowSizeY = 300;
+    int windowSizeX = 800;
+    int windowSizeY = 500;
 
     public StudyMain() {
-        Frame f = new Frame();
+        mainFrame = new Frame("StudyCards");
+//        mainFrame.setTitle("StudyCards");
+        centerWindow(mainFrame);
 
-        Button b = new Button("Click Me!!");
-        b.setBounds(30,100,80,30);
-        f.add(b);
+//        mainFrame.setLayout(new GridLayout(3, 1));
 
-        f.setSize(this.windowSizeX,this.windowSizeY);
-        f.setTitle("This is our basic AWT example");
+//        Button b = new Button("Click Me!!");
+//        b.setBounds(30,100,80,30);
+//        mainFrame.add(b);
 
-        centerWindow(f);
+        mainFrame.setSize(this.windowSizeX,this.windowSizeY);
 
-        // no layout manager
-        f.setLayout(null);
-        f.addWindowListener(
-            new WindowAdapter() {
-                 public void windowClosing(WindowEvent we) {
-                     f.dispose();
-                 }
+//        // no layout manager
+//        mainFrame.setLayout(null);
+//        mainFrame.addWindowListener(
+//            new WindowAdapter() {
+//                 public void windowClosing(WindowEvent we) {
+//                     mainFrame.dispose();
+//                 }
+//            }
+//        );
+        mainFrame.setLayout(new GridLayout(3, 1));
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
             }
-        );
+        });
+        headerLabel = new Label();
+        headerLabel.setAlignment(Label.CENTER);
+        statusLabel = new Label();
+        statusLabel.setAlignment(Label.CENTER);
+        statusLabel.setSize(350,100);
 
-        // now frame will be visible, by default it is not visible
-        f.setVisible(true);
+        controlPanel = new Panel();
+        controlPanel.setLayout(new FlowLayout());
+
+        mainFrame.setMenuBar(this.getMenuBar());
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true); // now frame will be visible, by default it is not visible
     }
 
     // Center the frame
@@ -41,5 +62,94 @@ public class StudyMain {
         int x = (screenSize.width - this.windowSizeX) / 2;
         int y = (screenSize.height - this.windowSizeY) / 2;
         f.setLocation(x, y);
+    }
+
+    private MenuBar getMenuBar() {
+        final MenuBar menuBar = new MenuBar();
+
+        //create menus
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        final Menu aboutMenu = new Menu("About");
+
+        //create menu items
+        MenuItem newMenuItem =
+                new MenuItem("New",new MenuShortcut(KeyEvent.VK_N));
+        newMenuItem.setActionCommand("New");
+
+        MenuItem openMenuItem = new MenuItem("Open");
+        openMenuItem.setActionCommand("Open");
+
+        MenuItem saveMenuItem = new MenuItem("Save");
+        saveMenuItem.setActionCommand("Save");
+
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setActionCommand("Exit");
+
+        MenuItem cutMenuItem = new MenuItem("Cut");
+        cutMenuItem.setActionCommand("Cut");
+
+        MenuItem copyMenuItem = new MenuItem("Copy");
+        copyMenuItem.setActionCommand("Copy");
+
+        MenuItem pasteMenuItem = new MenuItem("Paste");
+        pasteMenuItem.setActionCommand("Paste");
+
+        MenuItemListener menuItemListener = new MenuItemListener();
+
+        newMenuItem.addActionListener(menuItemListener);
+        openMenuItem.addActionListener(menuItemListener);
+        saveMenuItem.addActionListener(menuItemListener);
+        exitMenuItem.addActionListener(menuItemListener);
+        cutMenuItem.addActionListener(menuItemListener);
+        copyMenuItem.addActionListener(menuItemListener);
+        pasteMenuItem.addActionListener(menuItemListener);
+
+        final CheckboxMenuItem showWindowMenu =
+                new CheckboxMenuItem("Show About", true);
+        showWindowMenu.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if(showWindowMenu.getState()){
+                    menuBar.add(aboutMenu);
+                }else{
+                    menuBar.remove(aboutMenu);
+                }
+            }
+        });
+
+        //add menu items to menus
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(showWindowMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
+        editMenu.add(cutMenuItem);
+        editMenu.add(copyMenuItem);
+        editMenu.add(pasteMenuItem);
+
+        //add menu to menubar
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(aboutMenu);
+
+        //add menubar to the frame
+        mainFrame.setMenuBar(menuBar);
+        mainFrame.setVisible(true);
+
+        return menuBar;
+    }
+
+    class MenuItemListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(e.getActionCommand().equals("Exit")) {
+                System.exit(0);
+            }
+
+            statusLabel.setText(e.getActionCommand()
+                    + " MenuItem clicked.");
+        }
     }
 }
